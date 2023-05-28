6a182d369453053247668a94d5faf68933b30f02
Arunan131998 <arunan131998@gmail.com>
2023-05-28 13:19:21 +0600

    Logic to print a table

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int aTable = 5;
+        for(int i=1; i<=10;i++){
+            System.out.printf("%d * %d = %d",i,aTable,(i*aTable)).println();
+        }
     }
 }
